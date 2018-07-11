825addb556b2585fbfd7cc92416452f101c25e74
Vi_Krit <arbitrage13@gmail.com>
2018-07-11 15:58:40 +0700

    includes loadHistoricalData

--- asset.ts
+++ asset.ts
@@ -59,3 +59,49 @@ export class Options implements Asset{
         this.side = side;
     }
 }
+
+////////////////////////////////////////////////////////////////////////////////
+interface HistoricalData {
+    Date: string;
+    Open: number;
+    High: number;
+    Low: number;
+    Close: number;
+}
+
+function loadHistoricalData(filename: string): Promise<HistoricalData[]> {
+    return new Promise<HistoricalData[]>((resolve, reject) => {
+        let prices: HistoricalData[] = [];
+        csv.fromPath(filename, { headers: true}).on('data', (data: HistoricalData) => {
+            prices.push(data);
+        }).on('end', () => {
+            resolve(prices);
+        }).on('error', (error: any) => {
+            reject(error);
+        })
+    });
+}
+////////////////////////////////////////////////////////////////////////////////
+// Read data from file
+const INPUT_FILE = './input/EURUSD.csv';
+let fullPathFile: string = path.resolve(__dirname, INPUT_FILE);
+loadHistoricalData(fullPathFile).then((data: HistoricalData[]) => {
+let EURUSD = data; 
+function findMaxDate():string{
+    let max: number = 0;
+    let index:string = '';
+    for (let i of EURUSD){
+    if( i.High > max){
+        index = i.Date;}
+    }
+    return index; 
+}
+function findMax():number{
+    let max: number = 0;
+    for (let i of EURUSD){
+        if (i.High > max){
+        max = i.High;
+        }
+    }
+    return max;
+}
